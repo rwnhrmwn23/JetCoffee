53bc8ccfef981f89c93c965c76e94c06f1fb5f82
Irwan Hermawan <rwnhermawan@gmail.com>
2022-11-21 13:48:01 +0700

    add section banner, category, favorite, and best seller

--- app/src/main/java/com/onedev/jetcoffee/MainActivity.kt
+++ app/src/main/java/com/onedev/jetcoffee/MainActivity.kt
@@ -3,9 +3,28 @@ package com.onedev.jetcoffee
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.onedev.jetcoffee.model.Menu
+import com.onedev.jetcoffee.model.dummyBestSellerMenu
+import com.onedev.jetcoffee.model.dummyCategory
+import com.onedev.jetcoffee.model.dummyMenu
+import com.onedev.jetcoffee.ui.component.CategoryItem
+import com.onedev.jetcoffee.ui.component.HomeSection
+import com.onedev.jetcoffee.ui.component.MenuItem
+import com.onedev.jetcoffee.ui.component.SearchBar
 import com.onedev.jetcoffee.ui.theme.JetCoffeeTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,12 +39,75 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun JetCoffeeApp() {
-
+fun Banner(
+    modifier: Modifier = Modifier,
+) {
+    Box(modifier = modifier) {
+        Image(
+            painter = painterResource(R.drawable.banner),
+            contentDescription = "Banner Image",
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.height(160.dp)
+        )
+        SearchBar()
+    }
 }
 
-@Preview(showBackground = true, device = Devices.NEXUS_5)
 @Composable
+fun CategoryRow() {
+    LazyRow(
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+        contentPadding = PaddingValues(horizontal = 16.dp)
+    ) {
+        items(dummyCategory, key = { it.textCategory }) { category ->
+            CategoryItem(category)
+        }
+    }
+}
+
+@Composable
+fun MenuRow(
+    listMenu: List<Menu>,
+    modifier: Modifier = Modifier
+) {
+    LazyRow(
+        horizontalArrangement = Arrangement.spacedBy(16.dp),
+        contentPadding = PaddingValues(horizontal = 16.dp),
+        modifier = modifier
+    ) {
+        items(listMenu, key = { it.title }) { menu ->
+            MenuItem(menu)
+        }
+    }
+}
+
+@Composable
+fun JetCoffeeApp() {
+    Column(
+        modifier = Modifier.verticalScroll(rememberScrollState())
+    ) {
+        Banner()
+        HomeSection(
+            title = stringResource(R.string.section_category),
+            content = { CategoryRow() }
+        )
+        HomeSection(
+            title = stringResource(R.string.menu_favorite),
+            content = { MenuRow(dummyMenu) }
+        )
+        HomeSection(
+            title = stringResource(R.string.section_best_seller_menu),
+            content = { MenuRow(dummyBestSellerMenu) }
+        )
+    }
+}
+
+@Composable
+@Preview(
+    showSystemUi = true,
+    showBackground = true,
+    device = Devices.NEXUS_5,
+)
 fun JetCoffeeAppPreview() {
     JetCoffeeTheme {
         JetCoffeeApp()
---
+++ app/src/main/java/com/onedev/jetcoffee/ui/component/SearchBar.kt
@@ -0,0 +1,63 @@
+package com.onedev.jetcoffee.ui.component
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme.colors
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.material.TextFieldDefaults
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.onedev.jetcoffee.R
+import com.onedev.jetcoffee.ui.theme.JetCoffeeTheme
+
+@Composable
+fun SearchBar(
+    modifier: Modifier = Modifier,
+) {
+    TextField(
+        value = "",
+        onValueChange = {},
+        leadingIcon = {
+            Icon(
+                imageVector = Icons.Default.Search,
+                contentDescription = null
+            )
+        },
+        colors = TextFieldDefaults.textFieldColors(
+            backgroundColor = colors.surface,
+            disabledIndicatorColor = Color.Transparent,
+            focusedIndicatorColor = Color.Transparent,
+            unfocusedIndicatorColor = Color.Transparent,
+        ),
+        placeholder = {
+          Text(text = stringResource(R.string.placeholder_search))
+        },
+        modifier = modifier
+            .padding(16.dp)
+            .fillMaxWidth()
+            .heightIn(min =48.dp)
+            .clip(RoundedCornerShape(16.dp))
+    )
+}
+
+@Preview(
+    showBackground = true,
+    backgroundColor = 0xFF000000,
+)
+@Composable
+fun SearchBarPreview() {
+    JetCoffeeTheme {
+        SearchBar()
+    }
+}
